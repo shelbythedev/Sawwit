cc575d43ef7b6de0c473b1e5deab42ab9234c923
Shelby Solomon <shelby@shelbysolomon.net>
2017-05-09 19:27:40 -0500

    angular boilerplate added

---
+++ js/main.js
@@ -0,0 +1,23 @@
+var app = angular.module('sawwit', ['ngRoute'])
+
+//==== Routes ====
+app.config(['$routeProvider', function($routeProvider){
+  $routeProvider
+
+  // Root: Posts Index
+  .when('/', {templateUrl: 'partials/posts/post_index.html', controller: 'postsController'})
+
+  // else: 404 Error
+  .otherwise('/404', {templateUrl: 'partials/404.html', controller: 'appController'});
+}]);
+
+//==== Controllers ====
+// Main Application Controller
+app.controller('appController', ['$scope', '$log', function($scope, $log){
+  $log.debug('appController running');
+}]);
+
+// Posts Controller
+app.controller('postsController', ['$scope', '$log', function($scope, $log){
+  $log.debug('postsController running');
+}]);
