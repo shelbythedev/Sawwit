c5f12302b1d1a36f3164092f6b4de70315355c4f
Shelby Solomon <shelby@shelbysolomon.net>
2017-05-09 20:52:40 -0500

    added ngResource factories

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-var app = angular.module('sawwit', ['ngRoute'])
+var app = angular.module('sawwit', ['ngRoute', 'ngResource'])
 
 //==== Routes ====
 app.config(['$routeProvider', function($routeProvider){
